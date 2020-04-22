716ce9cfd1b46cde5ebf2272151452b9929283b1
Charlotte <s70848@beuth-hochschule.de>
2020-04-22 05:11:39 +0200

    adding sound to the game

--- simon-says/src/app/components/game/game.component.ts
+++ simon-says/src/app/components/game/game.component.ts
@@ -23,6 +23,7 @@ export class GameComponent{
  */
   running: boolean = false;
   count: number;
+  sounds: any = {};
   colors: any = {
     red: false,
     blue: false,
@@ -30,12 +31,42 @@ export class GameComponent{
     yellow: false
   };
 
-  constructor(private game: GameStateService) {}
+  constructor(private game: GameStateService) {
+    this.loadSounds();
+  }
 
+  /**
+  * @remarks
+  * This method loads all the sound files.
+  */
+  loadSounds(){
+    let audioUrl1 = '../../../assets/sounds/simonSound1.mp3';
+    let audioUrl2 = '../../../assets/sounds/simonSound2.mp3';
+    let audioUrl3 = '../../../assets/sounds/simonSound3.mp3';
+    let audioUrl4 = '../../../assets/sounds/simonSound4.mp3';
+    this.sounds= {
+      red: new Audio(audioUrl1),
+      blue: new Audio(audioUrl2),
+      green: new Audio(audioUrl3),
+      yellow: new Audio(audioUrl4)
+    };
+  }
+
+  /**
+  * @remarks
+  * This method plays specific sounds.
+  *
+  * @param color - Uses the color to get the sound.
+  *
+  */
+  playAudio(color: string){
+    this.sounds[color].play();
+  }
   /**
  * @remarks
  * This method initialises the game upon clicking the start button.
- *
+ * It also changes the "Start" on the start button to "Restart".
+ * If the game is already running it skips generating a Round and only restarts it.
  */
  onClick(){
    if(!this.running){
@@ -74,20 +105,22 @@ export class GameComponent{
  */
   playerGuess(e: string) {
     this.game.playerGuess(e);
+    this.playAudio(e);
   }
 
   /**
  * @remarks
- * This method, using the Promise method in {@file constants.ts}. sets the active color to true after waiting for 200ms
- * after waiting another 500ms it sets the active color to false again. The active colors are saved in the round array in {@file game-state.service.ts}
+ * This method, using the Promise method in {@file constants.ts}. sets the active color to true after waiting for 1000ms
+ * after waiting another 400ms it sets the active color to false again. The active colors are saved in the round array in {@file game-state.service.ts}
  *
  * @param round - A string array containing the colors of the going round.
  *
  */
   async teasePlayer(round: string[]) {
     for (let i = 0; i < round.length; i++) {
-      await sleep(700);
+      await sleep(1000);
       this.colors[round[i]] = true;
+      this.playAudio(round[i]);
       await sleep(400);
       this.colors[round[i]] = false;
     }
